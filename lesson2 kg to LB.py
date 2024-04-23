14dea02c969fba3195021cc6a646d1193970546b
samia8612 <41755524+samia8612@users.noreply.github.com>
2024-04-23 23:23:54 +0100

    Add files via upload

---
+++ lesson2 kg to LB.py
@@ -0,0 +1,5 @@
+print("This program will convert the wight from KG to LB")
+firstNum=int(input("Please enter the wight in KG: " ))
+secondNum=float(2.20462*firstNum)
+
+print(f"wight in LB :{secondNum}")
